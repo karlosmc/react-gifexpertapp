8b979182ad1ed3e6eba791eb26c810b4ed2a3a40
Carlos MAC <k4rlos.mc@gmail.com>
2020-11-03 19:26:02 -0500

    primer commit

---
+++ src/GifExpertApp.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
+
+export const GifExpertApp = () => {
+
+    //const categories =['One Punch','Samurai X','Dragon Ball'];
+    const [categories, setCategories] = useState(['One Punch']);
+
+  /*   const handleAdd = ()=>{
+        
+        //setCategories(categories.concat('HunterXHunter'));
+        //setCategories( [...categories,'HunterXHunter'] );
+        //setCategories( ['HunterXHunter',...categories] );
+        setCategories( cats => [...cats,'HunterXHunter'] );
+
+    } */
+
+    return (
+        <>
+            <h2>GifExpertApp</h2>
+            <AddCategory setCategories={setCategories}/>
+            <hr/>
+            
+                {
+                    categories.map( category =>(
+                        <GifGrid
+                        key={ category }
+                        category={ category }
+                      />  
+                    ))
+                }
+            
+        </>
+    )
+}
