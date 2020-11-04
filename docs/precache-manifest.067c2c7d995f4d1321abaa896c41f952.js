674656f787a5e3c28e63b3799a580a975710862a
Carlos MAC <k4rlos.mc@gmail.com>
2020-11-03 19:30:40 -0500

    docs agregado

---
+++ docs/precache-manifest.067c2c7d995f4d1321abaa896c41f952.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00d21497f9a87cd8248a567b8f578a3e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "12ea3cc35ee11c984b5f",
+    "url": "/static/css/main.fafaac0d.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12ea3cc35ee11c984b5f",
+    "url": "/static/js/main.281c0d06.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
